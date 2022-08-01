9a67d25e5d2a8b7258e0dd7ffdb382fce69cd477
Wasif <wasifanton@gmail.com>
2022-08-01 07:41:00 +0300

    adding new stuff

--- Hello.py
+++ Hello.py
@@ -1,2 +1,35 @@
-print("hello")
-print("hello")
+#print(), can you "" or '' 
+print("hello world")
+print('Hello world')
+print("\n")
+
+#creating variables
+x = 1 #int
+y = 1.0 #float
+z = "hi" #string
+
+print("this is an int:", x)
+print("this is a float:", y)
+print("this is a string:", z)
+print("\n")
+
+#casting, if you want to specify the data type of a variable
+x = int(1)
+y = float(1)
+z = str(1)
+
+print("this is an int:", x)
+print("this is a float:", y)
+print("this is a string:", z)
+print("\n")
+
+#type(), you can get the data type of a variable 
+x = 1
+y = 2.0
+z = "hi"
+
+print("x is:", type(x))
+print("y is:", type(y))
+print("z is:", type(z))
+print("\n")
+
